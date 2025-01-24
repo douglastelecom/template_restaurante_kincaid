9e709f5ee73a722500081dd93547bd22b6dd6ed2
Douglas <douglastelec@gmail.com>
2025-01-24 10:48:05 -0300

    antes da ia

---
+++ src/app/carrossel/carrossel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrossel',
+  imports: [],
+  templateUrl: './carrossel.component.html',
+  styleUrl: './carrossel.component.scss'
+})
+export class CarrosselComponent {
+
+}
